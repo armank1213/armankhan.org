debc708cfc391261a9ad8f2865ac9f3a7a2571be
Arman Khan <armank1006@gmail.com>
2024-09-26 16:37:09 -0700

    feat: Add LinkedIn profile link to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Twitter, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -16,6 +16,12 @@ const socials = [
 		href: "https://github.com/armank1213",
 		label: "Github",
 		handle: "armank1213",
+	},
+	{
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/arman-khan-479a9132a",
+		label: "LinkedIn",
+		handle: "Arman Khan",
 	},
 ];
 
